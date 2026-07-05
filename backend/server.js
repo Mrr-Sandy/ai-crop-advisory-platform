6c77e32e5769c787da794affa2922963c0b0edbf
Saurabh Singh <saurabhmasiyal345@gmail.com>
2026-07-05 21:35:05 +0530

    feat: connect backend with MongoDB Atlas using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const cropRoutes = require("./routes/cropRoute")
+const mongoose = require("mongoose");
+const cropRoutes = require("./routes/cropRoute");
+
 const app = express();
 
+
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 
 app.get("/", (req,res)=>{
